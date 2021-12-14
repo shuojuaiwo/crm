44748a89f1c0123db30997e66d90b9ae8b78d09c
芒果 <1919906937@qq.com>
2021-12-14 16:19:51 +0800

    李乾晨

---
+++ src/main/java/com/li/jtcrm/controller/controller.java
@@ -0,0 +1,13 @@
+package com.li.jtcrm.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class controller {
+    @RequestMapping("/toindex")
+    public String toindex(){
+        return "redirect:/system/index";
+    }
+
+}
