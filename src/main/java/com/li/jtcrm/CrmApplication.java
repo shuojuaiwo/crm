98d92ddc24a38f1777b7026517cf2df4025e436c
思念终将退散 <2748964757@qq.com>
2021-12-14 18:53:42 +0800

    张冲

--- src/main/java/com/li/jtcrm/CrmApplication.java
+++ src/main/java/com/li/jtcrm/CrmApplication.java
@@ -1,9 +1,11 @@
 package com.li.jtcrm;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@MapperScan("com.li")
 public class CrmApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/li/jtcrm/controller/controller.java
+++ src/main/java/com/li/jtcrm/controller/controller.java
@@ -1,10 +1,25 @@
 package com.li.jtcrm.controller;
 
+import com.li.jtcrm.entity.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 public class controller {
+
+    @RequestMapping("/login")
+    @ResponseBody
+    public Map login(User user){
+        Map map=new HashMap();
+        System.out.println(user);
+        map.put("success",1);
+        return map;
+    }
+
     @RequestMapping("/toindex")
     public String toindex(){
         return "system/index";
