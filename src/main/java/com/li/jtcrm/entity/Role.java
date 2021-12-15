f74a4cfcd718f1efc0780e34e6b037bc0fcce89a
思念终将退散 <2748964757@qq.com>
2021-12-15 14:50:15 +0800

    张冲

---
+++ src/main/java/com/li/jtcrm/entity/Role.java
@@ -0,0 +1,24 @@
+package com.li.jtcrm.entity;
+
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class Role {
+    private String id;
+
+    private Date createtime;
+
+    private String rolename;
+
+    private String rolenote;
+
+    private Long sortnum;
+
+    private BigDecimal status;
+
+    private Date updatetime;
+
+    private String createby;
+
+    private String updateby;
+}
