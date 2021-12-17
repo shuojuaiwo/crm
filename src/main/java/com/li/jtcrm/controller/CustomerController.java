9cba0c89cb509904684ab6d525aab2bc169bbe1e
思念终将退散 <2748964757@qq.com>
2021-12-17 10:56:18 +0800

    张冲

--- src/main/java/com/li/jtcrm/controller/CustomerController.java
+++ src/main/java/com/li/jtcrm/controller/CustomerController.java
@@ -3,7 +3,9 @@ package com.li.jtcrm.controller;
 import com.li.jtcrm.entity.Customer;
 import com.li.jtcrm.service.impl.CustomerServiceImpl;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
@@ -14,18 +16,6 @@ public class CustomerController {
     @Resource
     private CustomerServiceImpl customerService;
 
-    @RequestMapping("/toAddCustomer")
-    public String toAddCustomer(){
-        return "views/manage/customer/add";
-    }
-
-    @RequestMapping("/addCustomer")
-    @ResponseBody
-    public Map addCustomer(Customer customer){
-        Map map=customerService.addCustomer(customer);
-        return map;
-    }
-
     @RequestMapping("/toCustomer")
     public String toCustomer(){
         return "views/manage/customer/customer";
@@ -34,5 +24,25 @@ public class CustomerController {
     @RequestMapping("/toListCustomer")
     public String toListCustomer(){
         return "views/manage/customer/list";
+    }
+
+    @RequestMapping("/listCustomer")
+    @ResponseBody
+    public Map listCustomer(@RequestParam(defaultValue = "1") Integer pagenum, @RequestParam(defaultValue = "10") Integer size){
+        Map map=customerService.listCustomer(pagenum,size);
+        return map;
+    }
+
+    @RequestMapping("/toAddCustomer")
+    public String toAddCustomer(Model model){
+        customerService.toAddCustomer(model);
+        return "views/manage/customer/add";
+    }
+
+    @RequestMapping("/addCustomer")
+    @ResponseBody
+    public Map addCustomer(Customer customer){
+        Map map=customerService.addCustomer(customer);
+        return map;
     }
 }
--- src/main/java/com/li/jtcrm/service/ICustomerService.java
+++ src/main/java/com/li/jtcrm/service/ICustomerService.java
@@ -2,9 +2,14 @@ package com.li.jtcrm.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.li.jtcrm.entity.Customer;
+import org.springframework.ui.Model;
 
 import java.util.Map;
 
 public interface ICustomerService extends IService<Customer> {
     Map addCustomer(Customer customer);
+
+    Map listCustomer(Integer pagenum, Integer size);
+
+    void toAddCustomer(Model model);
 }
